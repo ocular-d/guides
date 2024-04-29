72c7841cf3405146896c698a7cab5fa4471894b9
sven <sven@leftxs.org>
2024-04-29 16:17:09 +0200

    wip - theme

--- guides/editorial/docusaurus.config.js
+++ guides/editorial/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Editorial',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Editorial',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
